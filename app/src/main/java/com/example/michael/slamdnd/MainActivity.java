7ffa7744502546637d5a13d55c7d0036cac48c00
agmaida1 <agmaida@knights.ucf.edu>
2015-11-06 21:49:47 -0500

    Pushing recent changes

--- app/src/main/java/com/example/michael/slamdnd/MainActivity.java
+++ app/src/main/java/com/example/michael/slamdnd/MainActivity.java
@@ -20,14 +20,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         Button loot = (Button)findViewById(R.id.lootBtn);
         loot.setOnClickListener(new View.OnClickListener(){
@@ -36,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LootActivity.class));
             }
         });
+
+        Button encounter = (Button)findViewById(R.id.encounterBtn);
+        encounter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, EncounterActivity.class));
+            }
+        });
+
     }
 
     @Override
